dfd661a94a5ee0f3c0b79f968d88a87d9f4dd316
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-11-01 22:20:52 +0100

    Build - add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/recipe/:slug">Recette</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <router-link to="/">
+      <img src="src/assets/logo.png" class="header-logo" alt="Logo oRecipes"/>
+    </router-link>
+  </header>
+</template>
+
+<script>
+import "../styles/_vars.scss";
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<style lang="scss">
+@use '../styles/vars';
+.header {
+  padding: vars.$halfGutter vars.$gutter;
+  box-shadow: 0 20px 20px -30px rgba(0, 0, 0, .4);
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &-logo {
+    width: 50px;
+  }
+}
+</style>
